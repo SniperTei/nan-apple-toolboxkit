7c8790412b90a00a435074f300e5f958b5e42b2a
Sniper <sniper_zheng@163.com>
2025-05-20 10:31:49 +0800

    把SDK转移到这里

---
+++ NanToolBoxKiit/NanToolBoxKiit/NanToolBoxKiit.h
@@ -0,0 +1,18 @@
+//
+//  NanToolBoxKiit.h
+//  NanToolBoxKiit
+//
+//  Created by zhengnan on 2025/5/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NanToolBoxKiit.
+FOUNDATION_EXPORT double NanToolBoxKiitVersionNumber;
+
+//! Project version string for NanToolBoxKiit.
+FOUNDATION_EXPORT const unsigned char NanToolBoxKiitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NanToolBoxKiit/PublicHeader.h>
+
+
