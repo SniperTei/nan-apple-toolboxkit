851113ecdbfc40bb1f9ee9ac661e2f0b035f8942
Sniper <sniper_zheng@163.com>
2025-05-22 16:29:51 +0800

    新创建

---
+++ SNPLogDemo/SNPLogDemo/SNPLog/SNPLogConfig.swift
@@ -0,0 +1,33 @@
+//
+//  SNPLogConfig.swift
+//  NanToolboxKit
+//
+//  Created by zhengnan on 2025/4/25.
+//
+
+import Foundation
+
+public struct SNPLogConfig {
+    public let logFilePath: String
+    public let logFileName: String
+    public let logLevel: SNPLogLevel
+    public let logType: SNPLogType
+    public let logInfoType: SNPLogInfoType
+    public let deviceId: String
+    
+    public init(
+        logFilePath: String,
+        logFileName: String,
+        logLevel: SNPLogLevel = .debug,
+        logType: SNPLogType = .console,
+        logInfoType: SNPLogInfoType = .info,
+        deviceId: String
+    ) {
+        self.logFilePath = logFilePath
+        self.logFileName = logFileName
+        self.logLevel = logLevel
+        self.logType = logType
+        self.logInfoType = logInfoType
+        self.deviceId = deviceId
+    }
+}
---
+++ SNPLogDemo/SNPLogDemo/TestLogController.swift
@@ -0,0 +1,20 @@
+//
+//  TestLogController.swift
+//  SNPLogDemo
+//
+//  Created by zhengnan on 2025/5/22.
+//
+
+import Foundation
+import UIKit
+
+class TestLogController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        print("test log controller")
+        
+    }
+}
