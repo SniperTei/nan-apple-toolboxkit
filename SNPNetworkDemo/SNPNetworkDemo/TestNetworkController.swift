019b87261e26d0f081abfb5784407aea1ac75291
Sniper <sniper_zheng@163.com>
2025-05-22 17:11:23 +0800

    init network

---
+++ SNPNetworkDemo/SNPNetworkDemo/TestNetworkController.swift
@@ -0,0 +1,16 @@
+//
+//  TestNetworkController.swift
+//  SNPNetworkDemo
+//
+//  Created by zhengnan on 2025/5/22.
+//
+
+import Foundation
+import UIKit
+
+class TestNetworkController: UIViewController {
+    
+    override func viewDidLoad() {
+        print("test network")
+    }
+}
---
+++ SNPNetworkDemo/SNPNetworkDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SNPNetworkDemo
+//
+//  Created by zhengnan on 2025/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
